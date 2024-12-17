8a76b331c4db22e340be5d21c0c2b4133248f2d7
Jhorky Escalante <jhorky2004@hotmail.com>
2024-12-17 15:08:21 -0300

    inicio del proyecto

---
+++ src/components/templates/homeTemplate.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export function HomeTemplate() {
+  return (
+    <Container>
+      <h1>Portfolio</h1>
+    </Container>
+  );
+}
+
+const Container = styled.div``;
